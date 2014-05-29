5c159d2a749c9a3d17c787aabefee146c887ef8f
bmehler <bernhard.mehler@gmail.com>
2014-05-29 21:19:52 +0200

    now with spaces

--- auto.php
+++ auto.php
@@ -3,26 +3,31 @@ class Auto
 {
 
     public $marke;
-	public $ps;
-	public $color;
+    public $ps;
+    public $color;
 	
 	
-	function __construct($marke = 'bmw', $ps = '55', $color = 'blau') {
-		$this->marke 	= $marke;
-		$this->ps		= $ps;
-		$this->color	= $blau;
-	}
+    function __construct($marke = 'bmw', $ps = '55', $color = 'blau')
+    {
+        $this->marke 	= $marke;
+        $this->ps		= $ps;
+        $this->color	= $blau;
+    }
 	
-	function getMarke() {
-		return $this->marke;
-	}
-	
-	function getPs(){
-		return $this->ps;
-	}
-	
-	function getColor(){
-		return $this->color;
-	}
+    function getMarke()
+    {
+        return $this->marke;
+    }
+
+    function getPs()
+    {
+        return $this->ps;
+    }
+
+    function getColor()
+    {
+        return $this->color;
+    }
 }
+
 ?>
