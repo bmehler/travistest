89166e3ad9940fdda6fabdaab1b105d469fc2aad
bmehler <bernhard.mehler@gmail.com>
2014-05-29 18:43:11 +0200

    first commit

---
+++ auto.php
@@ -0,0 +1,19 @@
+﻿<?php
+class Auto {
+
+	public $marke;
+	public $ps;
+	public $color;
+	
+	
+	function __construct($marke = 'bmw', $ps = '55', $color = 'blau') {
+		$this->marke 	= $marke;
+		$this->ps		= $ps;
+		$this->color	= $blau;
+	}
+	
+	function getMarke() {
+		return $this->marke;
+	}
+
+?>
