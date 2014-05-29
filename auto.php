ca3dd6b84de7079a5fddb34922d3d29002601aa2
bmehler <bernhard.mehler@gmail.com>
2014-05-29 21:26:41 +0200

    more spaces fixed

--- auto.php
+++ auto.php
@@ -5,15 +5,15 @@ class Auto
     public $marke;
     public $ps;
     public $color;
-	
-	
+
+
     function __construct($marke = 'bmw', $ps = '55', $color = 'blau')
     {
         $this->marke 	= $marke;
         $this->ps		= $ps;
         $this->color	= $blau;
     }
-	
+
     function getMarke()
     {
         return $this->marke;
@@ -29,5 +29,3 @@ class Auto
         return $this->color;
     }
 }
-
-?>
